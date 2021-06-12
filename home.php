357f2f33c7393c39ad24989404b0e33063c44d87
dhimas29 <dhimas.alvianrifita@gmail.com>
2021-06-12 11:15:45 +0700

    Home

--- home.php
+++ home.php
@@ -86,7 +86,7 @@
             <div class="row">
                 <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
                     <h1>BERINOVASI DAN MEMBANGUN</h1>
-                    <h2>Untuk mewujudkan visi ini, kami mempunyai tekad agar selalu bekerja sebaik mungkin dengan semboyan
+                    <h2 style="text-align: justify;">Untuk mewujudkan visi ini, kami mempunyai tekad agar selalu bekerja sebaik mungkin dengan semboyan
                         "DO THE BEST"</h2>
                     <!-- <div class="d-flex justify-content-center justify-content-lg-start">
                         <a href="#about" class="btn-get-started scrollto">Get Started</a>
