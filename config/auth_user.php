098323658c36fd5912fc049cf1a7a169556e4427
dhimas29 <dhimas.alvianrifita@gmail.com>
2021-06-12 11:14:35 +0700

    Home

--- config/auth_user.php
+++ config/auth_user.php
@@ -1,10 +1,8 @@
-<?php 
+<?php
 session_start();
 
 if (!isset($_SESSION['login'])) {
-	header('location:login.php');
+	header('location:home.php');
 	$_SESSION['pesan'] = "Anda harus login terlebih dahulu";
 	exit();
 }
-
- ?>
