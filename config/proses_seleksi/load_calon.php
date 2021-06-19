9cd84fad93902815ff1e86e95becbc44d2e71a72
dhimas29 <dhimas.alvianrifita@gmail.com>
2021-06-19 22:55:33 +0700

    Fixed Metode

--- config/proses_seleksi/load_calon.php
+++ config/proses_seleksi/load_calon.php
@@ -2,8 +2,7 @@
 
 $cek = mysqli_query($koneksi, "SELECT distinct calon.* FROM CALON 
 JOIN CALON_SUBKRITERIA ON (calon.id=CALON_SUBKRITERIA.calon_id)  
-join hasil_seleksi on (calon.id = hasil_seleksi.calon_id)
-where calon.divisi ='$_POST[session]' and hasil_seleksi.persetujuan != 'Disetujui'");
+where calon.divisi ='$_POST[session]' and calon.id not in (select calon_id from hasil_seleksi where persetujuan = 'Disetujui')");
 $row = mysqli_num_rows($cek);
 
 if ($row > 0) {
@@ -23,8 +22,7 @@ while ($data_row = mysqli_fetch_assoc($hasil)) {
 	FROM CALON_SUBKRITERIA JOIN SUBKRITERIA ON (SUBKRITERIA.ID=CALON_SUBKRITERIA.subkriteria_id) 
 	 join KRITERIA ON (KRITERIA.ID=SUBKRITERIA.kriteria_id) 
 	 join calon on (calon.id=calon_subkriteria.calon_id)
-	 join hasil_seleksi on (calon.id = hasil_seleksi.calon_id)
-	WHERE divisi ='$_POST[session]' and persetujuan != 'Disetujui' 
+	WHERE divisi ='$_POST[session]' and calon.id not in (select calon_id from hasil_seleksi where persetujuan = 'Disetujui') 
 	
 	and CALON_SUBKRITERIA.CALON_ID =" . $datas['data'][$data_row['id']]['id']);
 	$ro = mysqli_num_rows($ce);
@@ -34,8 +32,8 @@ while ($data_row = mysqli_fetch_assoc($hasil)) {
 	FROM CALON_SUBKRITERIA JOIN SUBKRITERIA ON (SUBKRITERIA.ID=CALON_SUBKRITERIA.subkriteria_id) 
 	 join KRITERIA ON (KRITERIA.ID=SUBKRITERIA.kriteria_id) 
 	 join calon on (calon.id=calon_subkriteria.calon_id)
-	 join hasil_seleksi on (calon.id = hasil_seleksi.calon_id)
-	WHERE divisi ='$_POST[session]' and persetujuan != 'Disetujui' and CALON_SUBKRITERIA.CALON_ID =" . $datas['data'][$data_row['id']]['id']);
+	 
+	WHERE divisi ='$_POST[session]' and calon.id not in (select calon_id from hasil_seleksi where persetujuan = 'Disetujui') and CALON_SUBKRITERIA.CALON_ID =" . $datas['data'][$data_row['id']]['id']);
 	} else {
 		$hasil2 = mysqli_query($koneksi, "SELECT KRITERIA.nama, KRITERIA.id, SUBKRITERIA.NAMA AS nama_subkriteria, 
 	SUBKRITERIA.BOBOT AS bobot_subkriteria, CALON_SUBKRITERIA.value 
